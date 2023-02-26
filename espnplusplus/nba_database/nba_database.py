744cc60676683e69ea3492f3a59c8ae7d3e73a1d
Dillon Evans <dillonje@uw.edu>
2023-02-25 19:44:25 -0800

    Replaced mysql_utils.py with mysql_handler.py.

--- espnplusplus/nba_database/nba_database.py
+++ espnplusplus/nba_database/nba_database.py
@@ -3,43 +3,40 @@ import mysql.connector
 import nba_api.stats.static.teams as nba_teams
 import nba_api.stats.static.players as nba_players
 
-import mysql_utils as utils
+from mysql_handler import MySQLHandler
 import nba_schema
 
-# FOR TESTING
-from time import sleep
-
-def create_teams_table(cursor):
+def create_teams_table(handler: MySQLHandler):
     table_description = nba_schema.TABLES['teams']
-    utils.create_table(cursor, 'teams', table_description)
+    handler.create_table('teams', table_description)
     teams = nba_teams.get_teams()
     for team in teams:
-        utils.insert_data(cursor, 'teams', team, team['full_name'])
+        handler.insert_data('teams', team, team['full_name'])
 
-def create_players_table(cursor):
+def create_players_table(handler: MySQLHandler):
     table_description = nba_schema.TABLES['players']
-    utils.create_table(cursor, 'players', table_description)
+    handler.create_table('players', table_description)
     active_players = nba_players.get_active_players()
     for player in active_players:
-        utils.insert_data(cursor, 'players', player, player['full_name'])
+        handler.insert_data('players', player, player['full_name'])
 
 if(__name__ == '__main__'):
-    cnx = mysql.connector.connect(
+    handler = MySQLHandler(
         host = 'localhost',
         user = 'root',
         password = 'password'
     )
-    cursor = cnx.cursor()
-    print('\nSTART\n')
-    input()
-    utils.use_database(cursor, nba_schema.DB_NAME)
-    cnx.commit()
-    print('\nNEXT: CREATING TEAMS TABLE\n')
-    input()
-    create_teams_table(cursor)
-    cnx.commit()
-    print('\nNEXT: CREATING PLAYERS TABLE\n')
-    input()
-    create_players_table(cursor)
-    cnx.commit()
-    cnx.close()
+    handler.drop_database('nba')
+    handler.close()
+
+    handler = MySQLHandler(
+        host = 'localhost',
+        user = 'root',
+        password = 'password',
+        autocommit = False
+    )
+    handler.use_database(nba_schema.DB_NAME)
+    create_teams_table(handler)
+    create_players_table(handler)
+    handler.commit()
+    handler.close()
